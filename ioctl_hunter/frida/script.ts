3ecb07f162175227cb6c2b2789c6ba8c5a5fd20b
cutecatsandvirtualmachines <145232977+cutecatsandvirtualmachines@users.noreply.github.com>
2025-10-01 14:41:15 +0200

    fix output buffer?

--- ioctl_hunter/frida/script.ts
+++ ioctl_hunter/frida/script.ts
@@ -1,23 +1,10 @@
-//
-// *LoadDriver symbols
-//
-
 var ZwLoadDriver = Module.findExportByName("ntdll.dll", 'ZwLoadDriver')
 var NtLoadDriver = Module.findExportByName("ntdll.dll", 'NtLoadDriver')
-
-//
-// *DeviceIoControl symbols
-//
 
 var DeviceIoControl_kernel32 = Module.findExportByName("kernel32.dll", 'DeviceIoControl')
 var DeviceIoControl_kernelbase = Module.findExportByName("kernelbase.dll", 'DeviceIoControl')
 var NtDeviceIoControlFile = Module.findExportByName("ntdll.dll", 'NtDeviceIoControlFile')
 var ZwDeviceIoControlFile = Module.findExportByName("ntdll.dll", 'ZwDeviceIoControlFile')
-
-
-//
-// *CreateFile* symbols
-//
 
 var CreateFileW_kernel32 = Module.findExportByName("kernel32.dll", 'CreateFileW')
 var CreateFileW_kernelbase = Module.findExportByName("kernelbase.dll", 'CreateFileW')
@@ -26,25 +13,13 @@ var CreateFileA_kernelbase = Module.findExportByName("kernelbase.dll", 'CreateFi
 var ZwCreateFile = Module.findExportByName("ntdll.dll", 'ZwCreateFile')
 var NtCreateFile = Module.findExportByName("ntdll.dll", 'NtCreateFile')
 
-//
-// *RegSetValue* symbols
-//
-
 var RegSetValueExW_advapi32 = Module.findExportByName("Advapi32.dll", 'RegSetValueExW')
-
-//
-// Dynamically called functions prototypes
-//
 
 var Exp_GetFinalPathNameByHandleW = Module.findExportByName("kernel32.dll", 'GetFinalPathNameByHandleW')
 var Func_GetFinalPathNameByHandleW = new NativeFunction(Exp_GetFinalPathNameByHandleW, 'int', ['int', 'pointer', 'int', 'int'])
 
 var Exp_NtQueryObject = Module.findExportByName("ntdll.dll", 'NtQueryObject')
 var Func_NtQueryObject = new NativeFunction(Exp_NtQueryObject, 'int', ['int', 'int', 'pointer', 'int', 'pointer'])
-
-//
-// Global variables
-//
 
 var x32 = false
 var all_symbols = false
@@ -54,10 +29,6 @@ var loaded_drivers = {}
 var queue_loaded_drivers = []
 var queue_device_ioctl = []
 var open_handles = {}
-
-//
-// RPC functions
-//
 
 rpc.exports = {
     exclude_ioctl: function (ioctl) {
@@ -88,35 +59,27 @@ rpc.exports = {
     },
 };
 
-//
-// Generic functions
-//
-
 function getPathByHandle(handle) {
     var ptr = Memory.alloc(2048)
     var ptr2 = Memory.alloc(2048)
     var ret = Func_NtQueryObject(handle, 1, ptr, 1024, ptr2)
 
     if (x32) {
-        var path = Memory.readUtf16String(ptr.add(8))   // 32 bits binaries
+        var path = Memory.readUtf16String(ptr.add(8))
     }
     else {
-        var path = Memory.readUtf16String(ptr.add(16))  // 64 bits binaries
+        var path = Memory.readUtf16String(ptr.add(16))
     }
 
     return path
 }
 
-//
-// *LoadDriver* interceptors
-//
-
 function LoadDriver_Manager(args) {
     if (x32) {
-        var loaded_driver = args[0].add(24).readUtf16String()   // 32 bits binaries
+        var loaded_driver = args[0].add(24).readUtf16String()
     }
     else {
-        var loaded_driver = args[0].add(16).readUtf16String()   // 64 bits binaries
+        var loaded_driver = args[0].add(16).readUtf16String()
     }
     var tmp_driver_name = loaded_driver.toLowerCase().split("\\")
 
@@ -142,7 +105,6 @@ Interceptor.attach(ZwLoadDriver, {
     },
 });
 
-
 Interceptor.attach(NtLoadDriver, {
     onEnter(args) {
         if (all_symbols) {
@@ -151,57 +113,62 @@ Interceptor.attach(NtLoadDriver, {
     },
 });
 
-
-//
-// *DeviceIoControl Interceptors
-//
-
-
-function DeviceIoControl_Manager(symbol_src, args, is_kernel_libs) {
+function DeviceIoControl_OnEnter_Manager(this_cpy, symbol_src, args, is_kernel_libs) {
     if (is_kernel_libs) {
-        var handle_device = args[0].toUInt32()
-        var ioctl = args[1].toUInt32()
-        var buff_in_addr = args[2]
-        var buff_in_size = args[3].toUInt32()
-        var buff_out_addr = args[4]
-        var buff_out_size = args[5].toUInt32()
-        var bytes_returned = Memory.readU32(args[6])
+        this_cpy.handle_device = args[0].toUInt32()
+        this_cpy.ioctl = args[1].toUInt32()
+        this_cpy.buff_in_addr = args[2]
+        this_cpy.buff_in_size = args[3].toUInt32()
+        this_cpy.buff_out_addr = args[4]
+        this_cpy.buff_out_size = args[5].toUInt32()
+        this_cpy.bytes_returned_ptr = args[6]
     }
     else {
-        var handle_device = args[0].toUInt32()
-        var ioctl = args[5].toUInt32()
-        var buff_in_addr = args[6]
-        var buff_in_size = args[7].toUInt32()
-        var buff_out_addr = args[8]
-        var buff_out_size = args[9].toUInt32()
-        var bytes_returned = ''
+        this_cpy.handle_device = args[0].toUInt32()
+        this_cpy.ioctl = args[5].toUInt32()
+        this_cpy.buff_in_addr = args[6]
+        this_cpy.buff_in_size = args[7].toUInt32()
+        this_cpy.buff_out_addr = args[8]
+        this_cpy.buff_out_size = args[9].toUInt32()
+        this_cpy.bytes_returned_ptr = null
     }
 
-    let hex_in = hexdump(buff_in_addr, {
-        offset: 0,
-        length: buff_in_size,
-        header: true,
-        ansi: false
-    })
-    let hex_out = hexdump(buff_out_addr, {
-        offset: 0,
-        length: buff_out_size,
-        header: true,
-        ansi: false
-    })
+    this_cpy.symbol_src = symbol_src
+    this_cpy.is_kernel_libs = is_kernel_libs
+}
 
-    if (hook_enabled && !(excluded_ioctl.includes(ioctl))) {
+function DeviceIoControl_OnLeave_Manager(this_cpy) {
+    if (hook_enabled && !(excluded_ioctl.includes(this_cpy.ioctl))) {
+        let hex_in = hexdump(this_cpy.buff_in_addr, {
+            offset: 0,
+            length: this_cpy.buff_in_size,
+            header: true,
+            ansi: false
+        })
+
+        var bytes_returned = ''
+        if (this_cpy.is_kernel_libs && this_cpy.bytes_returned_ptr) {
+            bytes_returned = Memory.readU32(this_cpy.bytes_returned_ptr)
+        }
+
+        let hex_out = hexdump(this_cpy.buff_out_addr, {
+            offset: 0,
+            length: this_cpy.buff_out_size,
+            header: true,
+            ansi: false
+        })
+
         let params = '{' +
-            ' "symbol":"' + symbol_src + '", ' +
-            ' "handle_device":"' + handle_device + '", ' +
-            ' "handle_path":"' + getPathByHandle(handle_device) + '", ' +
-            ' "ioctl":"' + ioctl + '", ' +
+            ' "symbol":"' + this_cpy.symbol_src + '", ' +
+            ' "handle_device":"' + this_cpy.handle_device + '", ' +
+            ' "handle_path":"' + getPathByHandle(this_cpy.handle_device) + '", ' +
+            ' "ioctl":"' + this_cpy.ioctl + '", ' +
             ' "buff_in": { ' +
-            ' "size":"' + buff_in_size + '", ' +
+            ' "size":"' + this_cpy.buff_in_size + '", ' +
             ' "hexdump":"' + encodeURI(hex_in) + '" ' +
             ' }, ' +
             ' "buff_out": { ' +
-            ' "size":"' + buff_out_size + '", ' +
+            ' "size":"' + this_cpy.buff_out_size + '", ' +
             ' "bytes_returned":"' + bytes_returned + '", ' +
             ' "hexdump":"' + encodeURI(hex_out) +
             '" } } ';
@@ -212,12 +179,16 @@ function DeviceIoControl_Manager(symbol_src, args, is_kernel_libs) {
     return false
 }
 
-
 Interceptor.attach(NtDeviceIoControlFile, {
     onEnter(args) {
         if (all_symbols) {
             var symbol_src = 'NtDeviceIoControlFile (ntdll.dll)'
-            DeviceIoControl_Manager(symbol_src, args, false)
+            DeviceIoControl_OnEnter_Manager(this, symbol_src, args, false)
+        }
+    },
+    onLeave(retval) {
+        if (all_symbols) {
+            DeviceIoControl_OnLeave_Manager(this)
         }
     },
 });
@@ -225,7 +196,10 @@ Interceptor.attach(NtDeviceIoControlFile, {
 Interceptor.attach(ZwDeviceIoControlFile, {
     onEnter(args) {
         var symbol_src = 'ZwDeviceIoControlFile (ntdll.dll)'
-        DeviceIoControl_Manager(symbol_src, args, false)
+        DeviceIoControl_OnEnter_Manager(this, symbol_src, args, false)
+    },
+    onLeave(retval) {
+        DeviceIoControl_OnLeave_Manager(this)
     },
 });
 
@@ -233,7 +207,12 @@ Interceptor.attach(DeviceIoControl_kernel32, {
     onEnter(args) {
         if (all_symbols) {
             var symbol_src = 'DeviceIoControl (kernel32.dll)'
-            DeviceIoControl_Manager(symbol_src, args, true)
+            DeviceIoControl_OnEnter_Manager(this, symbol_src, args, true)
+        }
+    },
+    onLeave(retval) {
+        if (all_symbols) {
+            DeviceIoControl_OnLeave_Manager(this)
         }
     },
 });
@@ -242,24 +221,21 @@ Interceptor.attach(DeviceIoControl_kernelbase, {
     onEnter(args) {
         if (all_symbols) {
             var symbol_src = 'DeviceIoControl (kernelbase.dll)'
-            DeviceIoControl_Manager(symbol_src, args, true)
+            DeviceIoControl_OnEnter_Manager(this, symbol_src, args, true)
+        }
+    },
+    onLeave(retval) {
+        if (all_symbols) {
+            DeviceIoControl_OnLeave_Manager(this)
         }
     },
 });
 
-//
-// *CreateFile* Interceptors
-//
-
 function ZwNt_CreateFile_Manager(args) {
     if (x32) {
-        // 32 bits binaries
-        // Pointer to OBJECT_ATTRIBUTES (ObjectAttributes) -> +8 PUNICODE_STRING (ObjectName) -> +4 PWSTR (Buffer)
         var path = args[2].add(8).readPointer().add(4).readPointer().readUtf16String()
     }
     else {
-        // 64 bits binaries
-        // Pointer to OBJECT_ATTRIBUTES (ObjectAttributes) -> +16 PUNICODE_STRING (ObjectName) -> +8 PWSTR (Buffer)
         var path = args[2].add(16).readPointer().add(8).readPointer().readUtf16String()
     }
     var handle = args[0].readPointer().toUInt32()
@@ -293,20 +269,6 @@ function Commons_CreateFile_OnLeave_Manager(this_cpy, retval) {
     };
     return true;
 }
-
-/*Interceptor.attach(ZwCreateFile, {
-    onEnter(args) {
-        ZwNt_CreateFile_Manager(args)
-    },
-});*/
-
-/*Interceptor.attach(NtCreateFile, {
-    onEnter(args) {
-        if (all_symbols) {
-            ZwNt_CreateFile_Manager(args)
-        }
-    },
-});*/
 
 Interceptor.attach(CreateFileW_kernel32, {
     onEnter(args) {
@@ -343,10 +305,6 @@ Interceptor.attach(CreateFileA_kernelbase, {
         Commons_CreateFile_OnLeave_Manager(this, retval)
     }
 });
-
-//
-// *RegSetValue* Interceptors
-//
 
 Interceptor.attach(RegSetValueExW_advapi32, {
     onEnter(args) {
